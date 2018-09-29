27eb1b7ff0a5bd2f7abeb665f6363404d5fc5e53
JLab <jessesoldat@gmail.com>
2018-09-29 17:34:28 -0500

    first commit all npm modules

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+const rootEl = document.getElementById("root");
+
+const render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    setTimeout(render);
+  });
+}
+
+registerServiceWorker();
